7a11b192d3c343fbf44bdddeb6dcff33d8a93363
AustinGirouard <57466429+AustinGirouard@users.noreply.github.com>
2020-11-30 21:11:30 -0600

    File Reformatting

--- src/Element.java
+++ src/Element.java
@@ -1,12 +1,14 @@
 package src;
 
 // self referential architecture
-class Element { 
-  String keyword;
-  Record head;
+class Element
+{ 
+    String keyword;
+    Record head;
 
-  Element(String k, Record r){
-    keyword = k;
-    head = r;
-  }
+    Element(String k, Record r)
+    {
+        keyword = k;
+        head = r;
+    }
 }
--- src/HashTableTest.java
+++ src/HashTableTest.java
@@ -5,85 +5,119 @@ import java.io.*;
 
 public class HashTableTest
 {
-  BufferedReader reader;
-  HashTableBuilder table;
-  int tableSize;
-public static void main(String[] args){HashTableTest test = new HashTableTest("datafile.txt");test.table.print();}
+    BufferedReader reader;
+    HashTableBuilder table;
+    int tableSize;
 
-  HashTableTest(String filename)
-  {
-    try
+    public static void main(String[] args)
     {
-      reader = new BufferedReader(new FileReader(filename)); // need to look at BufferedReader
-      ArrayList<String> tempArray = new ArrayList<String>();
-      FileData fd;
+        HashTableTest test = new HashTableTest("datafile.txt");
+        test.table.print();
+    }
 
-      while ((fd = this.readNextRecord()) != null) 
-      {
-        for (int i=0; i<fd.keywords.length; i++)
+    /*
+     * HashTableTest constructor
+     * Reads in all data from datafile.txt and inserts into HashTableBuilder
+     */
+    HashTableTest(String filename)
+    {
+        try
         {
-          if (!tempArray.contains(fd.keywords[i]))
-          {
-            tempArray.add(fd.keywords[i]);
-            tableSize++;
-          }
+            // need to look at BufferedReader
+            reader = new BufferedReader(new FileReader(filename));
+            ArrayList<String> tempArray = new ArrayList<String>();
+            FileData fd;
+
+            // While the datafile 
+            while ((fd = this.readNextRecord()) != null) 
+            {
+                for (int i = 0; i < fd.keywords.length; i++)
+                {
+                    if (!tempArray.contains(fd.keywords[i]))
+                    {
+                        tempArray.add(fd.keywords[i]);
+                        tableSize++;
+                    }
+                }
+            }
+            table = new HashTableBuilder(tableSize);
+            //System.out.printf("Number of Keywords: %d\n", tableSize);
+
+            reader = new BufferedReader(new FileReader(filename));
+            /* READS DATAFILE.TXT INTO DATASTRUCTURE  */
+            while ((fd = readNextRecord()) != null)
+            {
+                for (int i = 0; i < fd.keywords.length; i++)
+                {
+                    table.insert(fd.keywords[i], fd);
+                }
+            }
         }
-      }
-      table = new HashTableBuilder(tableSize);
-      //System.out.printf("Number of Keywords: %d\n", tableSize);
 
-      reader = new BufferedReader(new FileReader(filename));
-      /* READS DATAFILE.TXT INTO DATASTRUCTURE  */
-      while ((fd = readNextRecord()) != null){
-        for (int i=0; i<fd.keywords.length; i++){
-          table.insert(fd.keywords[i], fd);
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
-      }
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
-    finally {
-      try {
-        if (reader!=null) reader.close();
-      }
-      catch (IOException e){
-        e.printStackTrace();
-      }
-    }
-  }
 
-  public FileData readNextRecord(){
-		if(reader == null){
-			System.out.println("Error: You must open the file first.");
-			return null;
-		}
-		else{
-			FileData readData;
-			try{
-				String data=reader.readLine();
-				if(data==null) return null;
-                   // FileData(      int id,            String title,      String author,     int keywordCount)
-				readData= new FileData(Integer.parseInt(data), reader.readLine(), reader.readLine(), Integer.parseInt(reader.readLine()));
+        finally
+        {
+            try
+            {
+                if (reader != null) reader.close();
+            }
 
-				for (int i=0; i<readData.keywords.length; i++) {
-					readData.keywords[i] = reader.readLine();
-				}
-				String space=reader.readLine();
-				if((space!=null) && (!space.trim().equals(""))){
-					System.out.println("Error in file format");
-					return null;
-				}
-			}
-			catch(NumberFormatException e){
-				System.out.println("Error Number Expected! ");
-				return null;
-			}
-			catch(Exception e){
-				System.out.println("Fatal Error: "+e);
-				return null;
-			}
-			return readData;
-		}
-	}
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public FileData readNextRecord()
+    {
+        if(reader == null)
+        {
+            System.out.println("Error: You must open the file first.");
+            return null;
+        }
+
+        else
+        {
+            FileData readData;
+            
+            try
+            {
+                String data = reader.readLine();
+                if(data == null) return null;
+                //             FileData(      int id,            String title,      String author,     int keywordCount)
+                readData = new FileData(Integer.parseInt(data), reader.readLine(), reader.readLine(), Integer.parseInt(reader.readLine()));
+                
+                for (int i = 0; i < readData.keywords.length; i++)
+                {
+                    readData.keywords[i] = reader.readLine();
+                }
+                
+                String space = reader.readLine();
+                if((space != null) && (!space.trim().equals("")))
+                {
+                    System.out.println("Error in file format");
+                    return null;
+                }
+            }
+
+            catch(NumberFormatException e)
+            {
+                System.out.println("Error Number Expected! ");
+                return null;
+            }
+
+            catch(Exception e)
+            {
+                System.out.println("Fatal Error: " + e);
+                return null;
+            }
+
+            return readData;
+        }
+    }
 }
