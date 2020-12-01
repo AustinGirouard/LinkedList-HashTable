c64929be2387d295fbaf26a03cab433926c7c6a6
AustinGirouard <57466429+AustinGirouard@users.noreply.github.com>
2020-11-30 22:45:20 -0600

    Update HashTableBuilder.java
    Add comments

--- src/HashTableBuilder.java
+++ src/HashTableBuilder.java
@@ -5,17 +5,32 @@ class HashTableBuilder
     Element[] hashTable;
     int tableSize;
 
+    /*
+     * HashTableBuilder constructor.
+     * Creates a HashTable of size nextPrime(numKeyWords * 2)
+     */
     HashTableBuilder(int numKeyWords) 
     {
+        // Set tableSize and hashTable
         tableSize = nextPrime(numKeyWords * 2);
         hashTable = new Element[tableSize];
     }
 
+
+    /*
+     * This function creates a new element.
+     */
     private Element createNewElement(String keyword, Record recordToAdd) 
     {
         return new Element(keyword, recordToAdd);
     }
 
+
+    /*
+     * Helper method for insert(String keyword, Record recordToAdd)
+     * If the keyword already exists, the record is added directly,
+     * if the keyword does not exist, the main insert method is called.
+     */
     void insert(String keyword, FileData fd) 
     {
         Record recordToAdd = new Record(fd.id, fd.title, fd.author, null);
@@ -31,25 +46,39 @@ class HashTableBuilder
             recordToAdd.next = hashTable[index].head;
             hashTable[index].head = recordToAdd;
         }
-
-        /*if (!contains(keyword, recordToAdd)) {
-            insert(keyword, recordToAdd);*/
     }
 
+
+    /*
+     * This method adds the keyword to the HashTable and creates a new
+     * linkedList at that index.
+     */
     private void insert(String keyword, Record recordToAdd)
     {
+        // Convery keyword to integer representation
         int key = convertStringToInt(keyword);
-        //System.out.printf("%d %d\n", key, tableSize);
+
+        // Use modulo arithmetic to find index
         int index = key % tableSize;
+
+        // Set probe integer for collision handler
         int probe = 1;
+
+        // While collisions occur, rehash using quadratic probing
         while (hashTable[index] != null)
         {
             System.out.printf("%s conflicts with %s at index: %d\n", keyword, hashTable[index].keyword, index);
             index = getNextQuadProbIndex(key, probe++);
         }
+
+        // When open index is found, create new Element
         hashTable[index] = createNewElement(keyword, recordToAdd);
     }
 
+
+    /*
+     * COMMENT ME
+     */
     int find(String keyword)
     {
         int key = convertStringToInt(keyword);
@@ -66,11 +95,19 @@ class HashTableBuilder
         return -1;
     }
 
+
+    /*
+     *
+     */
     private int getNextQuadProbIndex(int key, int probe)
     {
         return ((key % tableSize) + (int) Math.pow(probe, 2)) % tableSize;
     }
 
+
+    /*
+     *
+     */
     private int convertStringToInt(String keyword)
     {
         int sum = 0;
@@ -81,27 +118,34 @@ class HashTableBuilder
         return sum;
     }
     
+
+    /*
+     *
+     */
     private int nextPrime(int num)
     {
         num++;
         for (int i = 2; i < num; i++)
         {
-            if(num%i == 0)
+            if(num % i == 0)
             {
                 num++;
-                i=2;
+                i = 2;
             }
 
             else
             {
-
-            continue;
+                continue;
             }
         }
 
         return num;
     }
 
+
+    /*
+     *
+     */
     public void print()
     {
         //int count = 0;
